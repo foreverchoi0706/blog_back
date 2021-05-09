277540c32eeab9e2926db4a55273fea3fe2c1a5e
foreverChoi <foreverchoi0706@fatoscorp.com>
2021-05-09 15:14:36 +0900

    update project

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export default {
+    db: {
+      address: "postgres://postgres:6007@52.78.180.169:5432/postgres",
+    },
+    api: {
+      key: "8642a281009d4d2ba8f54359d63eb348",
+    },
+  };
+  
