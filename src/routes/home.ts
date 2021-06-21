466de0a7d8e839201e038cd7d0f502f3ee2c07ab
foreverchoi <foreverchoi0706@gmail.com>
2021-06-22 01:38:10 +0900

    add get nickname api

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import axios from "axios";
 
 const home = express.Router();
 
 home.get("/", async (_, res: express.Response) => {
   res.header(200).json("welcome server");
+});
+
+home.get("/getNickname", async (_, res: express.Response) => {
+  const { data } = await axios.get("https://nickname.hwanmoo.kr/?format=text&count=1");
+  res.header(200).json({
+    nickname: data
+  });
 });
 
 export default home;
